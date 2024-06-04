811e3331c3aee149d240cc7a0217d0c4ed7959c0
rachelTeacher <rachel@haravdotnet.co.il>
2024-06-04 21:32:08 +0300

    aa

--- Logic/DBs/MyMoneyContext.cs
+++ Logic/DBs/MyMoneyContext.cs
@@ -42,8 +42,9 @@ public partial class MyMoneyContext : DbContext
     public virtual DbSet<UserType> UserTypes { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=MyMoney;user id=kollel;password=1234qwe!;TrustServerCertificate=true");
+      => optionsBuilder.UseSqlServer(@"Server=.;Database=MyMoneyB;Trusted_Connection=True;TrustServerCertificate=true;");
+
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
