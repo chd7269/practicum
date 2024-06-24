a6b62d726ec5933a3202b957557a640fd77049b6
E21427515m <e8515515@gmail.com>
2024-06-24 14:57:26 +0300

    calculate the tithe

--- Logic/Services/MovingService.cs
+++ Logic/Services/MovingService.cs
@@ -10,11 +10,13 @@ namespace Logic.Services
     public interface IMovingService
     {
         List<MovingDTO> GetMovings(Search search, int CurrentUserId);
-    //    object SumOfTithe();
+        //    object SumOfTithe();
 
         bool AddMove(MovingDTO move, int CurrentUserId);
         bool UpdateMove(MovingDTO move, int CurrentUserId);
         bool DeleteMove(int id, int CurrentUserId);
+
+        Tithe RepoTithe(int userId);
         Filters GetFilters(int type, int CurrentUserId);
 
     }
@@ -33,8 +35,8 @@ namespace Logic.Services
 
         }
         public List<MovingDTO> GetMovings(Search search, int CurrentUserId)
-        { 
-            
+        {
+
             List<MovingDTO> list = new List<MovingDTO>();
             bool isCurrentMonth = false;
             var query = dbService.entities.Movings.Where(x => x.User2Area.UserId == CurrentUserId).ToList();
@@ -205,26 +207,56 @@ namespace Logic.Services
             return filters;
         }
 
- //       public object  SumOfTithe()
-      //  {
-           
 
-         //   var query = from moving in dbService.entities.Movings
-                     //   join user2Area in dbService.entities.User2Areas
-                     //   on moving.User2AreaId equals user2Area.Id
-                     //   where user2Area.IsMaaser ==true
-                    //    select new
-                    //    {
-                          //  Moving = moving,
-                          ////  User2Area = user2Area
-                     //   };
+        public Tithe RepoTithe(int userId)
+        {
 
-            // Execute the query to retrieve the results
-         //   var result = query.ToList();
-         //   return result;
-     //   }
-
+            var month = DateTime.Now.Month;
        
+            var revenuesList = dbService.entities.Movings.Where(x => x.User2Area.UserId == userId && x.User2Area.Type == 1 && x.User2Area.IsMaaser == true && DateTime.Now.Month == x.Date.Month && DateTime.Now.Year == x.Date.Year).ToList();//&&date
+
+            var expensesList = dbService.entities.Movings.Where(x => x.User2Area.UserId == userId && x.User2Area.Type == 2 && x.User2Area.IsMaaser == true && DateTime.Now.Month == x.Date.Month && DateTime.Now.Year == x.Date.Year).ToList();//&&date
+
+            int sumOfRevenues = revenuesList.Sum(r => r.Sum);
+            int sumOfExpenses = expensesList.Sum(e => e.Sum);
+
+            Tithe tithe = new Tithe();
+            tithe.SumOfRevenues = sumOfRevenues;
+            tithe.SumOfExpenses = sumOfExpenses;
+            tithe.DateTithe = new DateTime(DateTime.Now.Year,DateTime.Now.Month,1);
+            return tithe;
+
+        }
+
+            //ליצור נתון של האם זה הוצאה קבועה בתחומי הוצאות
+            //DTO : הכנסות, הוצאות, חודש ושנה ת מעשרות קבועות
+            //06.2024   25000    15000 
+            //pipe שעושה את חישוב המעשר לחודש זה
+
+
+
+         
+        
+        //       public object  SumOfTithe()
+        //  {
+
+
+        //   var query = from moving in dbService.entities.Movings
+        //   join user2Area in dbService.entities.User2Areas
+        //   on moving.User2AreaId equals user2Area.Id
+        //   where user2Area.IsMaaser ==true
+        //    select new
+        //    {
+        //  Moving = moving,
+        ////  User2Area = user2Area
+        //   };
+
+        // Execute the query to retrieve the results
+        //   var result = query.ToList();
+        //   return result;
+        //   }
+
+
 
 
         //public bool SetMoveToUser2Sub()
@@ -254,6 +286,6 @@ namespace Logic.Services
 
 
         //    return true;
-        //}
-    }
+        }
+    
 }
