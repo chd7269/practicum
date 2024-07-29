f202bb626b18e8373c1136195e9fdb252d32bdfc
rachelGesher <64225419+rachelGesher@users.noreply.github.com>
2024-07-29 12:40:16 +0300

    fix debts

--- Logic/DBs/History.cs
+++ Logic/DBs/History.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logic.DTO;
+using System;
 using System.Collections.Generic;
 
 namespace Logic;
@@ -18,8 +19,8 @@ public partial class History
     public int? OldAmount { get; set; }
 
     public int? NewAmount { get; set; }
+    public ActionOptions ActionOption { get; set; }  
 
-    public int? ActionOption { get; set; }
 
     public virtual User User { get; set; } = null!;
 }
--- Logic/DTO/ExpandedRevenuesDTO.cs
+++ Logic/DTO/ExpandedRevenuesDTO.cs
@@ -8,16 +8,14 @@ namespace Logic.DTO
 {
     public class PresenceSettingsDTO
     {
-        public int PresenceId { get; set; }
-        public int? UserId { get; set; }
+        public int Id { get; set; }
         public int Day { get; set; }
         public int Hours { get; set; }
     }
 
     public class AmountSettingsDTO
     {
-        public int ProductId { get; set; }
-        public int? UserId { get; set; }
+        public int Id { get; set; }
         public int Day { get; set; }
         public string Product { get; set; }
         public string ProductType { get; set; }
